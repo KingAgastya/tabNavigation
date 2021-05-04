31c420df00358fa6da89b1bbf4bd3f9ef75e5b2c
KingAgastya <63898735+KingAgastya@users.noreply.github.com>
2021-05-04 10:14:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Facebook from './screens/Facebook'
+import Instagram from './screens/Instagram'
 
 export default function App() {
   return (
